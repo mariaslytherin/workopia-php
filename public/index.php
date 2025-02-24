d92355e8013c2b429e82ec9db35ee609ff9f04fc
mariaslytherin <maria.pirinova39@gmail.com>
2025-02-24 21:51:08 +0200

    Refactor router functionality

--- public/index.php
+++ public/index.php
@@ -1,6 +1,11 @@
 <?php
 require '../helpers.php';
+require basePath('Router.php');
+
+$router = new Router();
+$routes = require basePath('routes.php');
 
 $uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
 
-require basePath('router.php');
+$router->route($uri, $method);
