20851e6270fd2c1d21ecc106f5331bdce592a313
mariaslytherin <maria.pirinova39@gmail.com>
2025-02-26 23:24:35 +0200

    Implement validation for listing submission

--- routes.php
+++ routes.php
@@ -3,4 +3,5 @@
 $router->get('/', 'HomeController@index');
 $router->get('/listings', 'ListingController@index');
 $router->get('/listings/create', 'ListingController@create');
+$router->post('/listings', 'ListingController@store');
 $router->get('/listing/{id}', 'ListingController@show');
